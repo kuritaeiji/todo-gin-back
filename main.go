108dc941d11ef80b0378b987318f0be56401728b
kuritaeiji <eiji199825@yahoo.co.jp>
2022-05-23 12:52:05 +0900

    releaseモードの時のみseedデータを作成

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/kuritaeiji/todo-gin-back/config"
 	"github.com/kuritaeiji/todo-gin-back/db"
 	"github.com/kuritaeiji/todo-gin-back/seed"
@@ -13,6 +14,8 @@ func main() {
 	db.Init()
 	defer db.CloseDB()
 	validators.Init()
-	seed.CreateSeedData()
+	if gin.Mode() == gin.ReleaseMode {
+		seed.CreateSeedData()
+	}
 	server.Init()
 }
